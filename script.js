7dcd91c3cfc7e1ef82b5356206d24989c95bc5a6
Aniket Yadav <117531261+aniket0657@users.noreply.github.com>
2025-10-07 20:54:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const parseCode = (str) => {
-  // your code here
+  // Split the string using one or more zeros as the separator
+  const parts = str.split(/0+/);
+
+  // Return the object with required properties
+  return {
+    firstName: parts[0],
+    lastName: parts[1],
+    id: parts[2]
+  };
 };
 
 // Do not change the code below
